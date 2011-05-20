5a3721da38d35b8db6faf8253f5b7547da86a477
arshan <arshan@localhost>
2011-05-20 09:56:06 -0700

    updated license info

--- arduino/drawesome/libraries/ADK/ADK.cpp
+++ arduino/drawesome/libraries/ADK/ADK.cpp
@@ -1,9 +1,17 @@
 /*
-  ADK.cpp - Library for interfacing with Android device
-  Created by Buck Clay, April 12, 2011.
-  Copyright 2011 Google Inc. All Rights Reserved.
-
-  TODO(arshan): update all the license info
+ * Copyright (C) 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 #include "ADK.h"
